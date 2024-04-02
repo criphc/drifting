4f8edf7672fb9a31d07b5436185e9f2bab07e490
Criphc <criphc@outlook.com>
2024-04-02 08:47:14 +0800

    test script

--- content/micro/new.fish
+++
@@ -1,8 +0,0 @@
-set content "+++
-title = \"$argv\"
-template = \"404.html\"
-+++"
-set nowdate "$(date +%Y-%m-%d-%H%M%S)"
-set name "$nowdate.md"
-cd /Users/criphc/Sites/drifting/content/micro/
-echo $content > $name
